86afb500d6c8560d28b45c07d09ed9e596c03e39
Hassanjadi <hassanjadi25@gmail.com>
2024-08-04 00:30:25 +0700

    first project

---
+++ Materi/firstProject/HelloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// bisa running menggunakan run go <nama file>
+// bisa running menggunakan run build
+// step: run build, dir, ./<nama folder>
