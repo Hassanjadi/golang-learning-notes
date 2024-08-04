7115793289a73678e104323ce0437ef494138212
Hassanjadi <hassanjadi25@gmail.com>
2024-08-04 13:38:11 +0700

    materi tipe data

---
+++ Materi/tipe-data/try.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	var positiveNumber uint8 = 89
+	var negativeNumber = -1243423644
+
+	fmt.Println("bilangan positif: %d\n", positiveNumber)
+	fmt.Println("bilangan negatif: %d\n", negativeNumber)
+}
