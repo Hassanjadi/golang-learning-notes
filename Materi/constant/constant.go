68375cd34f15c2a57fbe4d35bf7ed08dec11e946
Hassanjadi <hassanjadi25@gmail.com>
2024-08-04 13:37:46 +0700

    materi const/constant

---
+++ Materi/constant/constant.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const judul = "Ini Judul"
+	fmt.Println(judul)
+
+	// jika kode dibawah ini di uncomment maka akan error
+	// judul = "judul ini di ubah"
+}
